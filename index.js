18995e9091e2034fcc463e7c92e3cae819212e97
Prajwal Ladkat <praju.ladkat@gmail.com>
2022-11-17 13:23:01 +0530

    updated cors

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ dotenv.config({
 dotenv.config();
 const app = express();
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "*",
     credentials: true
 }))
 app.use(express.json());
